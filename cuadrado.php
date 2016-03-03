6f597a4bcfe68bb4f3f73f3b9dc706cc1955a24b
lupitaaa <sabjg.95@gmail.com>
2016-03-03 15:51:42 -0600

    practica4

---
+++ cuadrado.php
@@ -0,0 +1,5 @@
+<?php
+ $numero=$_POST["numero"];
+ $resultado=$numero * $numero;
+  echo "<h1>".$resultado."</h1>";
+?>
---
+++ formulario.php
@@ -0,0 +1,12 @@
+<html>
+ <body>
+  <form action="operacion.php"
+     method="POST">
+    <label> numero </label>
+    <input type="text"
+      name="numero">
+    <input type="submit"
+      value="calcular">
+  </form>
+ </body>
+</html> 
